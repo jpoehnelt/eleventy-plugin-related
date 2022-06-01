86c2f889a65683e33eddc45d571ebf1f17c23219
Justin Poehnelt <jpoehnelt@google.com>
2022-06-01 14:45:32 -0600

    fix: enable caching if same document array

--- src/related.ts
+++ src/related.ts
@@ -1,10 +1,25 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {Related, Options} from "related-documents";
 
 export const related = (options: Options) => {
+  let related: Related;
 
   return (document: any, documents: any[]): {absolute: number, relative: number, document: any}[] => {
-    let related: Related;
-
     if (!related || !shallowIsEqual(documents, related.documents)) {
       related = new Related(documents, options);
     }
